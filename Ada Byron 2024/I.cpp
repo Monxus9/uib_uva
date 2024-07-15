376f8be3f06129a86387b2802460349053f283c1
Monxus9 <92452322+Monxus9@users.noreply.github.com>
2024-07-15 19:54:41 +0200

    Add files via upload

--- Ada Byron 2024/I.cpp
+++ Ada Byron 2024/I.cpp
@@ -10,6 +10,10 @@ int n_nodos, n_aristas, max_edificios;
 int ori_inicio, dest_final;
 vector<vector<pair<int, pair<int, int>>>> nodos; // {destination, {time, buildings}}
 vector<double> ratios;
+vector<double> memo;
+vector<bool> visitado; 
+double max_min_time;
+
 
 class Ruta {
     public:
@@ -24,6 +28,41 @@ class Ruta {
             return ratio < other.ratio;
         }
 };
+
+
+
+/*
+double dfs(double rat, int idx_nodo) {
+    //cout << "Visito: " << idx_nodo << endl;
+    if (idx_nodo == dest_final) return 0;
+    if (memo[idx_nodo] != -INF) return memo[idx_nodo];
+
+    visitado[idx_nodo] = true;
+    //cout << "Entro: " << idx_nodo << endl;
+
+    double max_weight = -INF;
+    for (int i = 0; i < nodos[idx_nodo].size(); i++)
+    {
+        int next_idx = nodos[idx_nodo][i].first;
+        if (visitado[next_idx]) continue;
+        double weight = nodos[idx_nodo][i].second.second - (rat * nodos[idx_nodo][i].second.first);
+        double total_weight = weight + dfs(rat, next_idx);
+        if (total_weight > max_weight) {
+            max_weight = total_weight;
+        }
+    }
+    visitado[idx_nodo] = false;
+    memo[idx_nodo] = max_weight;
+    return memo[idx_nodo];
+}
+
+double calculaGrafo(double rat) {
+    memo = vector<double>(n_nodos+1, -INF);
+    visitado = vector<bool>(n_nodos+1, false);
+
+    return dfs(rat, ori_inicio);
+}*/
+
 
 double calculaGrafo(double rat) {
     priority_queue<Ruta> rutas;
@@ -44,19 +83,21 @@ double calculaGrafo(double rat) {
         // cout << "MR: " << myruta.idxNodo << " " << myruta.ratio << endl;
         rutas.pop();
         int idx = myruta.idxNodo;
-        if (idx == dest_final) break;
+        if (idx == dest_final) continue;
         if (visitados.find(idx) != visitados.end()) continue;
         visitados.insert(idx);
 
+        double current_weight = myruta.ratio;
         for (int i = 0; i < nodos[idx].size(); i++)
         {
             double weight = nodos[idx][i].second.second - (rat * nodos[idx][i].second.first);
             int next_nodo = nodos[idx][i].first;
-            if (ratios[idx] + weight > ratios[next_nodo]) {
-                ratios[next_nodo] = ratios[idx] + weight;
+            if (current_weight + weight > ratios[next_nodo]) {
+                ratios[next_nodo] = current_weight + weight;
+                rutas.push({next_nodo, ratios[next_nodo]});
                 
             }
-            rutas.push({next_nodo, ratios[next_nodo]});
+            
         }
 
     }
@@ -65,9 +106,10 @@ double calculaGrafo(double rat) {
     return ratios[dest_final];
 }
 
+
 double busquedaBinaria(double left, double right) {
     double mid = 0;
-    while (right - left > 1e-7) {
+    while (right - left > 4e-7) {
         mid = (right + left) / 2;
         if (calculaGrafo(mid) >= 0) {
             left = mid;
